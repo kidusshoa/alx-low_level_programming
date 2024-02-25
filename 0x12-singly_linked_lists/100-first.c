d19f43b1c3e48e87a54fddb76c98d1d2908eb963
kidusshoa <captainkevin2008@gmail.com>
2024-02-25 10:45:11 +0300

    linked list3

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 void __attribute__((constructor)) before_main(void);
 
 /**
- * before_main - function that prints a message before main is executed
+ * before_main - Function that prints a message before main is executed.
  */
 void before_main(void)
 {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - The main function.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("In the main function.\n");
+    return 0;
 }
 
